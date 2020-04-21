cdf49bdf3343a98f3901606dd98fdfb4b219a89b
Shan Wong <shan.wong29@gmail.com>
2020-04-21 11:05:45 +0200

    Updates

---
+++ precache-manifest.725df7124b169b0c08d9b44e2509de40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eec940727a7b019dd78960e01c8606ee",
+    "url": "/roman-num-calc/index.html"
+  },
+  {
+    "revision": "3c9369f6bed3a9b54f6e",
+    "url": "/roman-num-calc/static/css/main.83a7e514.chunk.css"
+  },
+  {
+    "revision": "61fb94d1dcb080be35f7",
+    "url": "/roman-num-calc/static/js/2.d98434b4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/roman-num-calc/static/js/2.d98434b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c9369f6bed3a9b54f6e",
+    "url": "/roman-num-calc/static/js/main.58764c95.chunk.js"
+  },
+  {
+    "revision": "3d0d54502403d3136f42",
+    "url": "/roman-num-calc/static/js/runtime-main.4fe1fbc5.js"
+  }
+]);
