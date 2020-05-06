41fd24e5481f07849b68a7cceb3544acba38d9dc
Shan Wong <shan.wong29@gmail.com>
2020-05-06 10:28:30 +0200

    Add conditional logic to modeControl to avoid rerendering

--- src/Component/ModeControl/ModeControl.js
+++ src/Component/ModeControl/ModeControl.js
@@ -3,15 +3,26 @@ import Button from "../Button/Button";
 import Context from "../../store/context";
 
 const ModeControl = () => {
-  const { dispatch } = useContext(Context);
+  const { state, dispatch } = useContext(Context);
 
   let modeBtns = [`arabic`, `roman`];
+
+  console.log("Mode Control");
 
   modeBtns = modeBtns.map((el, i) => {
     return (
       <Button
         key={i}
-        onClickFn={() => dispatch({ type: `CHANGE_LANG_MODE`, mode: el })}
+        onClickFn={() => {
+          if (
+            (el === `roman` && state.isRomanMode) ||
+            (el === `arabic` && !state.isRomanMode)
+          ) {
+            return;
+          } else {
+            dispatch({ type: `CHANGE_LANG_MODE` });
+          }
+        }}
       >
         {el}
       </Button>
